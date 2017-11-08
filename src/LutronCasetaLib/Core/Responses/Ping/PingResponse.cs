df43375bcfa32fbb1a9ce51cc289c87856147798
Chris Berthold <cberthold@technodotnet.com>
2017-11-08 00:08:30 -0500

    add json objects for ping response

---
+++ src/LutronCasetaLib/Core/Responses/Ping/PingResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LutronCaseta.Core.Responses.Ping
+{
+    public partial class PingResponse
+    {
+        [JsonProperty("LEAPVersion")]
+        public double LEAPVersion { get; set; }
+    }
+}
---
+++ src/LutronCasetaLib/Core/Responses/Ping/PingResponseBody.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LutronCaseta.Core.Responses.Ping
+{
+    public partial class PingResponseBody : IResponseBody
+    {
+        [JsonProperty("PingResponse")]
+        public PingResponse PingResponse { get; set; }
+    }
+}
