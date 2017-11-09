5db640c5541d5aaed0e67f0b12c789e132e5ccdb
Chris Berthold <cberthold@technodotnet.com>
2017-11-09 01:11:45 -0500

    add commands that were originally hard coded to connector

---
+++ src/LutronCasetaLib/Commands/GetDevicesCommand.cs
@@ -0,0 +1,16 @@
+﻿using LutronCaseta.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LutronCaseta.Commands
+{
+    public class GetDevicesCommand : AbstractWriteCommand
+    {
+        public override void SendCommand(IWriteProcessor processor)
+        {
+            string deviceCommand = "{\"CommuniqueType\":\"ReadRequest\",\"Header\":{\"Url\":\"/device\"}}\n";
+            processor.WriteString(deviceCommand);
+        }
+    }
+}
---
+++ src/LutronCasetaLib/Commands/PingCommand.cs
@@ -0,0 +1,16 @@
+﻿using LutronCaseta.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LutronCaseta.Commands
+{
+    public class PingCommand : AbstractWriteCommand
+    {
+        public override void SendCommand(IWriteProcessor processor)
+        {
+            string ping = "{\"CommuniqueType\":\"ReadRequest\",\"Header\":{\"Url\":\"/server/1/status/ping\"}}\n";
+            processor.WriteString(ping);
+        }
+    }
+}
