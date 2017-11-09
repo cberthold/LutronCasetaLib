f7a2ec80f0c4264e67e87c35ef41856c07c30466
Chris Berthold <cberthold@technodotnet.com>
2017-11-09 01:15:51 -0500

    add extension methods to make using the connector easy to use and maintain the existing API

---
+++ src/LutronCasetaLib/Extensions/ConnectorExtensions.cs
@@ -0,0 +1,23 @@
+﻿using LutronCaseta.Commands;
+using LutronCaseta.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LutronCaseta
+{
+    public static class ConnectorExtensions
+    {
+        public static void Ping(this IWriteProcessor processor)
+        {
+            var command = new PingCommand();
+            processor.ExecuteCommand(command);
+        }
+
+        public static void GetDevices(this IWriteProcessor processor)
+        {
+            var command = new GetDevicesCommand();
+            processor.ExecuteCommand(command);
+        }
+    }
+}
