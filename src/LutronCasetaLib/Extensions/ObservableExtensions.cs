6a0c504c3abf1971e6e595d1c2860361e26a2995
Chris Berthold <cberthold@technodotnet.com>
2017-11-13 23:26:02 -0500

    add observable extension to wrap an observable into an async task with timeout

---
+++ src/LutronCasetaLib/Extensions/ObservableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LutronCaseta
+{
+    public static class ObservableExtensions
+    {
+        public static Task<T> WaitFor<T>(this IObservable<T> source, Func<T, bool> pred, int timeoutMs = 1000, CancellationToken token = default(CancellationToken))
+        {
+            T returnValue = default(T);
+            try
+            {
+                return
+                    source
+                        .Where(pred)
+                        .Timeout(TimeSpan.FromMilliseconds(timeoutMs))
+                        .DistinctUntilChanged()
+                        .Take(1)
+                        .ToTask(token);
+            }
+            catch
+            {
+                return Task.FromResult(returnValue);
+            }
+        }
+    }
+}
